acd00bd5bfec246a6a9a5e702de375554132ad72
Jessica Duell <duellrdh@gmail.com>
2018-08-23 20:21:39 -0400

    Shows ID# when mouse hovers over div and removes text when mouse leaves

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Creates a button that when clicked creates a new div to the page
 let addSquare = document.createElement('button');
 let divContainer = document.createElement('div');
 let text = document.createTextNode("Click Here");
@@ -5,3 +6,25 @@ let text = document.createTextNode("Click Here");
 addSquare.appendChild(text);
 document.body.appendChild(addSquare);
 document.body.appendChild(divContainer);
+
+let id = 1; 
+let colors = ['black','blue', 'purple', 'yellow', 'green', 'brown', 'aqua', 'pink', 'red'];
+
+//Function that when the button is pressed a new div is put on the page with an unique id 
+addSquare.addEventListener('click', function (){
+    let div = document.createElement('div');
+    let divText = document.createTextNode("ID: " + id);
+    div.className = "square";
+    divContainer.appendChild(div);
+    id++;
+
+    div.addEventListener('mouseover', function (){
+        div.appendChild(divText);
+    })
+
+    div.addEventListener('mouseleave', function(){
+        divText.remove();
+    })
+});
+
+//Function that when the square is clicked the background color changes to a random color from the array of colors
