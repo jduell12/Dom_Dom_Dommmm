17f28c937be5f0ddee17cb3d6873a93052dbf261
Jessica Duell <duellrdh@gmail.com>
2018-08-23 20:07:09 -0400

    created Button

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let addSquare = document.createElement('button');
+let divContainer = document.createElement('div');
+let text = document.createTextNode("Click Here");
+
+addSquare.appendChild(text);
+document.body.appendChild(addSquare);
+document.body.appendChild(divContainer);
